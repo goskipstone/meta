99a61e4fb23fd1836d42a742a01746b9f1995a53
Kareem Janoudi <kjanoudi@gmail.com>
2016-06-13 18:36:04 -0400

    Default exports

--- src/getMediaSrc.js
+++ src/getMediaSrc.js
@@ -1,6 +1,6 @@
 import { AWS_STORAGE_TYPES } from './constants'
 import { default as constructPath } from './constructPath'
 
-export function getMediaSrc(media, organizationId) {
+export default function getMediaSrc(media, organizationId) {
   return constructPath(media, AWS_STORAGE_TYPES.Media, organizationId)
 }
